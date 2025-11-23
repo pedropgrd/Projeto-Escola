cec7cba6bd52b1d353c9130d1106da768fd93f13
Wagner Ferreira <wvinicius.torres@gmail.com>
2025-11-22 20:39:42 -0400

    melhoria

--- front-angular/src/environments/environment.prod.ts
+++ front-angular/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@
  */
 export const environment = {
   production: true,
-  apiUrl: 'https://api.seudominio.com',
+  apiUrl: 'http://72.61.124.30/trajano',
   tokenKey: 'auth_token',
   refreshTokenKey: 'refresh_token',
   userKey: 'user_data'
